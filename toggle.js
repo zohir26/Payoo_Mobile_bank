f7d729755d47d71823e53fad20eddb0e85b77288
zohir <jahirraihan500@gmail.com>
2024-09-21 23:12:31 +0600

    comment added

--- toggle.js
+++ toggle.js
@@ -1,6 +1,10 @@
+// when cashout button will clicked the hidden property or class will be removed for cashout-form and add hidden class for add-money-form 
+
 document.getElementById("cashout-button").addEventListener('click', function(){
     document.getElementById("cashout-form").classList.remove("hidden")
     document.getElementById("addmoney-form").classList.add("hidden")
+
+// when add money button will be clicked the hidden property will be removed for add-money-form and hidden property will be add for cashout-form.
 
     document.getElementById("addmoney-button").addEventListener('click' , function(){
         document.getElementById("addmoney-form").classlist.remove("hidden");
